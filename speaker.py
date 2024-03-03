f1d6c08e034880a36b0a044051226c6a15121c1c
noda-sin <noda.sin@gmail.com>
2024-03-03 17:39:59 +0900

    Refactor speaker

--- speaker.py
+++ speaker.py
@@ -5,15 +5,21 @@ import os
 
 import simpleaudio
 
-def speak(text: str, speaker=43):
-    resp = requests.post(f"http://localhost:50021/audio_query?speaker={speaker}&text={text}")
-    resp = requests.post(f"http://localhost:50021/synthesis?speaker={speaker}", json=resp.json())
-    with tempfile.TemporaryFile(mode='wb', delete=False) as f:
-        f.write(resp.content)
-    wav_obj = simpleaudio.WaveObject.from_wave_file(f.name)
-    play_obj = wav_obj.play()
-    play_obj.wait_done()
-    os.unlink(f.name)
+class Speaker:
+    def __init__(self, speaker_id=43) -> None:
+        self.speaker_id = speaker_id
+
+
+    def speak(self, text: str):
+        resp = requests.post(f"http://localhost:50021/audio_query?speaker={self.speaker_id}&text={text}")
+        resp = requests.post(f"http://localhost:50021/synthesis?speaker={self.speaker_id}", json=resp.json())
+        with tempfile.TemporaryFile(mode='wb', delete=False) as f:
+            f.write(resp.content)
+        wav_obj = simpleaudio.WaveObject.from_wave_file(f.name)
+        play_obj = wav_obj.play()
+        play_obj.wait_done()
+        os.unlink(f.name)
+
 
 if __name__ == "__main__":
-    speak("こんにちは")
+    Speaker().speak("こんにちは")
